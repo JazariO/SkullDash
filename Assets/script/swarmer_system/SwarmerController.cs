98ec4a44e24a3e5bbd69bd8d2c6d4added3e7ce3
JazariO <djazz5112@gmail.com>
2025-12-17 17:00:10 +1100

    add(SwarmerController.cs)

---
+++ Assets/script/swarmer_system/SwarmerController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class SwarmerController : MonoBehaviour
+{
+    [SerializeField] float correction_rate = 0.25f;
+
+    [SerializeField] float input_speed = 10f;
+    [SerializeField] float turn_speed = 15f;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] SphereCollider coll;
+
+    [SerializeField] Transform target;
+    private Vector3 target_direction;
+
+    private void FixedUpdate()
+    {
+        // calculate target direction
+        target_direction = target.position - rb.position;
+
+        Vector3 corrected_target_vector = Vector3.Lerp(transform.forward, target_direction, correction_rate);
+        corrected_target_vector.Normalize();
+
+        float direction_dot = Vector3.Dot(rb.transform.forward, target_direction);
+
+        Vector3 cross_torque = Vector3.Cross(rb.transform.forward, target_direction).normalized * turn_speed;
+        rb.AddTorque(cross_torque);
+
+        rb.AddForce(corrected_target_vector * input_speed, ForceMode.Force);
+        if(rb.linearVelocity.sqrMagnitude != 0.0f)
+        {
+            transform.rotation = Quaternion.LookRotation(rb.linearVelocity, Vector3.up);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(target.position, 0.1f);
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(rb.position, target.position);
+
+        Gizmos.color = Color.lightGreen;
+        Gizmos.DrawWireSphere(coll.transform.position, coll.radius);
+    }
+}
