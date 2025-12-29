c31a37eb91f798c5b4c2edfe8ed5b32de00599aa
JazariO <djazz5112@gmail.com>
2025-12-29 15:56:09 +1100

    change(PlayerCameraController.cs): add camera tilt

--- Assets/script/player_system/PlayerCameraController.cs
+++ Assets/script/player_system/PlayerCameraController.cs
@@ -23,16 +23,16 @@ public class PlayerCameraController : MonoBehaviour
         player_stats_SO.tempStats.curPitch = Mathf.Clamp(player_stats_SO.tempStats.curPitch, -player_settings_SO.clampedPitch, player_settings_SO.clampedPitch);
         pivot_camera_end_effector.localRotation = Quaternion.Euler(player_stats_SO.tempStats.curPitch, 0, 0);
 
-        // Handle player movement tilt.
-        // Absolute Dot product between player velocity and camera pivot left direction.
+        // Handle player movement tilt
         {
-            // NOTE(Jazz): get player maxspeed for inverse lerp
-            //float tilt_target = Mathf.InverseLerp(player_settings_SO.runSpeed, , player_stats_SO.tempStats.moveVelocity.magnitude);
-            //if(Mathf.Abs(tilt_target) < 0.01f) tilt_target = 0f;
+            float tilt_target = Mathf.InverseLerp(0f, player_settings_SO.runSpeed, player_stats_SO.tempStats.speed);
+            if(Mathf.Abs(tilt_target) < 0.01f) tilt_target = 0f;
 
-            //Debug.Log("tilt_dot: " + tilt_target);
-            //tilt_current = Mathf.Lerp(tilt_current, tilt_target, tilt_acceleration * Time.deltaTime);
-            //pivot_camera_tilt_link.localRotation = Quaternion.Euler(0, 0, tilt_current * tilt_scalar);
+            float tilt_target_direction = -Vector3.Dot(player_stats_SO.tempStats.moveVelocity, pivot_camera_end_effector.right);
+            tilt_target *= tilt_target_direction;
+
+            tilt_current = Mathf.Lerp(tilt_current, tilt_target, tilt_acceleration * Time.deltaTime);
+            pivot_camera_tilt_link.localRotation = Quaternion.Euler(0, 0, tilt_current * tilt_scalar);
         }
     }
 }
